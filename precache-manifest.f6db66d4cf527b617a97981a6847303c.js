b22d142fb96b54728cdf44cd8ed66f1124bca7bc
RavilAxmadyllin <craftwaymiks@gmail.com>
2020-09-18 15:55:15 +0300

    Updates

---
+++ precache-manifest.f6db66d4cf527b617a97981a6847303c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e973222b6d302c115054c47d7d9bf83",
+    "url": "/decks/index.html"
+  },
+  {
+    "revision": "e4ad76af1b38468ba53a",
+    "url": "/decks/static/css/main.df8ba3fe.chunk.css"
+  },
+  {
+    "revision": "6aa1a184a875ec084469",
+    "url": "/decks/static/js/2.8232ddcb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/decks/static/js/2.8232ddcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4ad76af1b38468ba53a",
+    "url": "/decks/static/js/main.ba47932f.chunk.js"
+  },
+  {
+    "revision": "1cd2594ad23f64643a14",
+    "url": "/decks/static/js/runtime-main.1d53e5bc.js"
+  },
+  {
+    "revision": "2da04171bf70a9310f4ffb1ffc006465",
+    "url": "/decks/static/media/avatar.2da04171.png"
+  }
+]);
